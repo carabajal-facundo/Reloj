39366f7d9befbe40737fb250527ba49885e64020
carabajal-facundo <facarabajal@hotmail.com>
2023-04-11 11:45:01 -0300

    Correccion de la muestra de segundos

--- js/program.js
+++ js/program.js
@@ -1,16 +1,15 @@
-setInterval(setearFecha,1000);
-let fecha = new Date();
-let dias = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'];
-let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
-            'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
-
-let pFecha = document.getElementById('pFecha');
-let hora = document.getElementById('hora');
-let minutos = document.getElementById('minutos');
-let segundos = document.getElementById('segundos');
-let tipo = document.getElementById('tipo');
 
 function setearFecha(){
+    let fecha = new Date();
+    let dias = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'];
+    let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+                'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+    
+    let pFecha = document.getElementById('pFecha');
+    let hora = document.getElementById('hora');
+    let minutos = document.getElementById('minutos');
+    let segundos = document.getElementById('segundos');
+    let tipo = document.getElementById('tipo');
     pFecha.textContent = `${dias[fecha.getDay()-1]} ${fecha.getDate()} de ${meses[fecha.getMonth()]} del ${fecha.getFullYear()}`;
     minutos.textContent = fecha.getMinutes();
     segundos.textContent = fecha.getSeconds();
@@ -23,3 +22,6 @@ function setearFecha(){
         tipo.textContent = 'AM';
     }
 }
+
+
+window.setInterval(setearFecha,1000);
